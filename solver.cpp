8e3d19b24cafda9792b5722f86740b81b6ebdb82
Wattanai Sathuphan <nine.stay27@gmail.com>
2018-03-16 02:17:24 +0700

    cut() completed

--- solver.cpp
+++ solver.cpp
@@ -5,6 +5,7 @@ int ans[10][10]; //actually can use only char but I use int for easy to get inpu
 void printAns()
 {
      //Print ans
+    cout << "Answer table" << endl;
     for(int i=1;i<10;i++)
     {
         for(int j=1;j<10;j++)
@@ -14,6 +15,7 @@ void printAns()
         cout << endl;
     }
     //Print tbl
+    cout << "Calculation table" << endl;
     for(int i=1;i<10;i++)
     {
         for(int j=1;j<10;j++)
@@ -28,21 +30,42 @@ int grouping(int c)
     if(c<=3)
         return 1;
     else if(c>=7)
-        return 3;
+        return 7;
     else
-        return 2;
+        return 4;
 }
 void cut(int x,int y,int a)
 {
+    tbl[x][y][0] = 0;
     for(int i=1;i<10;i++)
     {
-        tbl[i][y][0]--;
-        tbl[i][y][a] = 0;
+        if (tbl[i][y][a] != 0 && tbl[i][y][0] != 0)
+        {
+            tbl[i][y][0]--;
+            tbl[i][y][a] = 0;
+        }
     }
     for(int i=1;i<10;i++)
     {
-        tbl[x][i][0]--;
-        tbl[x][i][a] = 0;
+        if(tbl[x][i][a] != 0 && tbl[x][i][0] != 0)
+        {
+            tbl[x][i][0]--;
+            tbl[x][i][a] = 0;
+        }
+    }
+    //cut in 3x3 group
+    int i = grouping(x);
+    int j = grouping(y);
+    for(int ii=i;ii<=i+2;ii++)
+    {
+        for(int jj=j;jj<=j+2;jj++)
+        {
+            if(tbl[ii][jj][a] != 0 && tbl[ii][jj][0] != 0)
+            {
+                tbl[ii][jj][0]--;
+                tbl[ii][jj][a] = 0;
+            }
+        }
     }
 }
 int main()
@@ -56,6 +79,7 @@ int main()
                 tbl[i][j][k] = 1;
         }
     }
+    //printAns();
     for(int i=1;i<10;i++)
     {
         for(int j=1;j<10;j++)
